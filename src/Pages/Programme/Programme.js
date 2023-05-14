9b12d9b9b34439f12e7d77f00341a810300c37bb
RichaBharti <richabharti154@gmail.com>
2023-05-14 16:55:00 +0530

    Proramming.js

--- src/Pages/Programme/Programme.js
+++ src/Pages/Programme/Programme.js
@@ -1,14 +1,31 @@
 import React from 'react'
+import StripeCheckout from 'react-stripe-checkout';
 import {useState} from 'react'
 import style from '../../Pages/Programme/Programme.module.css'
 export default function Programme(src,alt){
     const [hovered, setHovered] = useState(false);
+    const [donationAmount, setDonationAmount] = useState(0);
     function handleMouseEnter(){
         setHovered(true)
     }
     function handleMouseLeave(){
          setHovered(false)
     }
+    function handleToken(token) {
+        // Make a request to your backend to process the donation
+        fetch('/charge', {
+          method: 'POST',
+          body: JSON.stringify(token),
+        })
+          .then((response) => {
+            // Handle successful donation
+          })
+          .catch((error) => {
+            // Handle donation error
+          });
+    
+        }
+            
     return(
         <section className={style.program1}>
             <div className={style.programContainer}>
@@ -19,32 +36,39 @@ export default function Programme(src,alt){
             <div className={style.programmetitle}>Education to every child</div>
             <div className={style.donationCount}>Donation Goal:<span>$9845</span>
             </div>
-            <button className={style.button1}>Donate Now</button>
+            <button className={style.button1} onClick={() => setDonationAmount(10006)}>Donate Now</button>
            </div>
            <div className={style.box} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             <div  className={`${style.cardImage1} ${hovered ? style.zoom : ''}`} src={src} alt={alt}></div>
             <div className={style.programmetitle}>Dedication to helping child</div>
             <div className={style.donationCount}>Donation Goal:<span>$9845</span>
             </div>
-            <button  className={style.button1}>Donate Now</button>
+            <button  className={style.button1}onClick={() => setDonationAmount(4520)}>Donate Now</button>
            </div>
            <div className={style.box} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             <div className={`${style.cardImage2} ${hovered ? style.zoom : ''}`} src={src} alt={alt}></div>
             <div className={style.programmetitle}>Make life easier for them</div>
             <div className={style.donationCount}>Donation Goal:<span>$9845</span>
             </div>
-            <button className={style.button1}>Donate Now</button>
+            <button className={style.button1} onClick={() => setDonationAmount(60000)}>Donate Now</button>
            </div>
            <div className={style.box} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
             <div className={`${style.cardImage3} ${hovered ? style.zoom : ''}`} src={src} alt={alt}></div>
             <div className={style.programmetitle}> clean water for people</div>
             <div className={style.donationCount}>Donation Goal:<span>$9845</span>
             </div>
-            <button className={style.button1}>Donate Now</button>
+            <button className={style.button1} onClick={() => setDonationAmount(20000)}>Donate Now</button>
+            
            </div>
            </div>
            </div>
-           <div>
+           <div className={style.stripe}>
+           <StripeCheckout 
+              token={handleToken}
+              stripeKey="your_stripe_api_key"
+              amount={donationAmount * 100}
+              currency="USD"
+            />
              </div>
       </section>
     )
